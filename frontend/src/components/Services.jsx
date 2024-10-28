cea9b9a4f84cd43a8460ead6a80c287fca779876
Your Name <85418443+alrammahi-mariia@users.noreply.github.com>
2024-10-28 20:11:46 +0200

    fetch image url from the services content type and render image in the frontend

--- frontend/src/components/Services.jsx
+++ frontend/src/components/Services.jsx
@@ -4,28 +4,34 @@ import { fetchContent } from "../services/api";
 const Services = () => {
   const [content, setContent] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [imageUrl, setImageUrl] = useState("");
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    // fetchContent("/images-api")
-    //   .then((data) => {
-    //     console.log("Fetched data:", data); // Log the fetched data
-    //     setContent(data[0]); // Access the first item in the data array
-    //     setLoading(false);
-    //   })
-    //   .catch((error) => {
-    //     console.error("Error fetching image:", error); // Log any errors
-    //     setError(error);
-    //     setLoading(false);
-    //   });
-    fetch("https://localhost:62786/jsonapi/images-api/?includes=name")
-      .then((response) => response.json())
+    fetchContent(
+      "node/services?include=field_image_file&fields[file--file]=uri,url"
+    )
       .then((data) => {
         console.log("Fetched data:", data); // Log the fetched data
-        setContent(data); // Access the first item in the data array
+        console.log("Image url:", data.included[0].attributes.uri.url); // Log the fetched data
+        setContent(data.data[0]); // Access the first item in the data array
+        setImageUrl(data.included[0].attributes.uri.url); // Access the image url and set it to imageUrl state
         setLoading(false);
       })
-      .catch((imgError) => console.error("Error fetching image:", imgError));
+      .catch((error) => {
+        console.error("Error fetching image:", error); // Log any errors
+        setError(error);
+        setLoading(false);
+      });
+
+    //   fetch("https://localhost:62786/jsonapi/images-api")
+    //     .then((response) => response.json())
+    //     .then((data) => {
+    //       console.log("Fetched data:", data); // Log the fetched data
+    //       setContent(data); // Access the first item in the data array
+    //       setLoading(false);
+    //     })
+    //     .catch((imgError) => console.error("Error fetching image:", imgError));
   }, []);
 
   if (loading) {
@@ -39,10 +45,10 @@ const Services = () => {
   return (
     <div>
       <h1>Services</h1>
-      {content && content.length > 0 ? (
-        <div>
-          {content.map((item, index) => {
-            const imageUrl = item.field_media_image[0].url;
+      {/* {content && content.length > 0 ? ( */}
+      <div>
+        <img src={`https://localhost:62786/${imageUrl}`} />
+        {/* {content.map((item, index) => {
             return (
               <div key={index}>
                 {imageUrl ? (
@@ -52,11 +58,8 @@ const Services = () => {
                 )}
               </div>
             );
-          })}
-        </div>
-      ) : (
-        <div>No content available</div>
-      )}
+          })} */}
+      </div>
     </div>
   );
 };
