71da1e2a06b19606333ec0adf7deddb60a5cfd70
Your Name <85418443+alrammahi-mariia@users.noreply.github.com>
2024-10-28 11:09:10 +0200

    update localhost in api.jsx

--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:51431/jsonapi/";
+const API_URL = "https://localhost:62786/jsonapi";
 
 export const fetchContent = async (contentType) => {
   const response = await axios.get(`${API_URL}/${contentType}`);
