a17dfe8a7926f87b5a7571bcd70cf641b5f13343
Your Name <85418443+alrammahi-mariia@users.noreply.github.com>
2024-10-28 23:54:11 +0200

    fetch images and image urls from media module endpoint and render them

--- frontend/src/components/Services.jsx
+++ frontend/src/components/Services.jsx
@@ -4,18 +4,18 @@ import { fetchContent } from "../services/api";
 const Services = () => {
   const [content, setContent] = useState(null);
   const [loading, setLoading] = useState(true);
-  const [imageUrl, setImageUrl] = useState("");
+  const [imageData, setImageData] = useState([]);
   const [error, setError] = useState(null);
 
   useEffect(() => {
     fetchContent(
-      "node/services?include=field_image_file&fields[file--file]=uri,url"
+      "media/image?include=field_media_image&fields[file--file]=uri,url" //include a uuid in the request to get specific image
     )
       .then((data) => {
         console.log("Fetched data:", data); // Log the fetched data
-        console.log("Image url:", data.included[0].attributes.uri.url); // Log the fetched data
+        console.log("Image url:", data.included); // Log the fetched data
         setContent(data.data[0]); // Access the first item in the data array
-        setImageUrl(data.included[0].attributes.uri.url); // Access the image url and set it to imageUrl state
+        setImageData(data.included); // Access the image url array and set it to imageData state in an array
         setLoading(false);
       })
       .catch((error) => {
@@ -34,6 +34,8 @@ const Services = () => {
     //     .catch((imgError) => console.error("Error fetching image:", imgError));
   }, []);
 
+  console.log("image data", imageData);
+
   if (loading) {
     return <div>Loading...</div>;
   }
@@ -45,20 +47,15 @@ const Services = () => {
   return (
     <div>
       <h1>Services</h1>
-      {/* {content && content.length > 0 ? ( */}
+      {/* // <img src={`https://localhost:62786/${imageUrl}`} /> */}
       <div>
-        <img src={`https://localhost:62786/${imageUrl}`} />
-        {/* {content.map((item, index) => {
-            return (
-              <div key={index}>
-                {imageUrl ? (
-                  <img src={imageUrl} />
-                ) : (
-                  <div>No image available</div>
-                )}
-              </div>
-            );
-          })} */}
+        {imageData.map((item, index) => {
+          return (
+            <div key={index}>
+              <img src={`https://localhost:62786${item.attributes.uri.url}`} />
+            </div>
+          );
+        })}
       </div>
     </div>
   );
