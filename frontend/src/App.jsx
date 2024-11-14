10533be6d8c3235d5aa0253754870df97df16f50
Bishnu Suyel <suyel2007@gmail.com>
2024-11-14 08:41:20 +0200

    Revert "Create a hamburger menu"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
-import React, { useEffect } from "react";
-import { Routes, Route, useLocation } from "react-router-dom";
-import mautic from "./services/mautic_services";
+import React from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import Layout from "./pages/Layout";
 import About from "./pages/About";
@@ -8,48 +7,27 @@ import Contact from "./pages/Contact";
 import Services from "./pages/Services";
 import ServiceSingle from "./pages/ServiceSingle";
 import ServicePage from "./pages/ServiceSingle";
-import Blog from "./pages/Blog";
-import Career from "./pages/Career";
 
 const App = () => {
-  const location = useLocation();
-
-  useEffect(() => {
-    // Update page title dynamically based on the path
-    const pageTitles = {
-      "/": "Homepage",
-      "/services": "Our Services",
-      "/services/:single": "Service Single Page",
-      "/about": "About Us",
-      "/contact": "Contact",
-    };
-    const title = pageTitles[location.pathname] || "Default Title";
-    document.title = title;
-
-    // Track the page view in Mautic
-    mautic.pageView({
-      path: location.pathname,
-      title: document.title,
-    });
-  }, [location]);
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route>
-          {/* This is where other routes will go to allow Layout to be visible everywhere */}
-          <Route path="/" element={<Home />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/services/:single" element={<ServiceSingle />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/career" element={<Career />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/service-single" element={<ServicePage />} />
-        </Route>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route>
+            {/* This is where other routes will go to allow Layout to be visible everywhere */}
+            <Route path="/" element={<Home />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/services/:single" element={<ServiceSingle />} />
+            <Route path="/about" element={<About />} />
+            {/* <Route path="/blog" element={<Blog />} /> */}
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/service-single" element={<ServicePage />} />
+          </Route>
 
-        {/* <Route path="*" element={<ErrorPage />} /> */}
-      </Route>
-    </Routes>
+          {/* <Route path="*" element={<ErrorPage />} /> */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
--- frontend/src/pages/Layout.jsx
+++ frontend/src/pages/Layout.jsx
@@ -1,21 +1,67 @@
 import React from "react";
-import { Container, Row } from "react-bootstrap";
-import { Outlet } from "react-router-dom";
-import HamburgerMenu from "../components/HamburgerMenu";
+import { Link, Outlet } from "react-router-dom";
+import { Navbar, Nav, Container, Row } from "react-bootstrap";
 
 const Layout = () => {
   return (
     <div className="d-flex flex-column min-vh-100">
       <Container fluid>
         <Row>
-          {/* Hamburger Menu Component */}
-          <HamburgerMenu />
+          <Navbar
+            bg="light"
+            variant="light"
+            expand={false}
+            fixed="top"
+            className="px-2 px-md-4"
+          >
+            {" "}
+            {/* Setting expand={false} */}
+            {/* Left-aligned Logo */}
+            <Navbar.Brand as={Link} to="/" className="logo me-auto">
+              <img
+                src="/logo.svg"
+                alt="Druid Logo"
+                style={{ width: "50px", height: "auto" }}
+              />
+            </Navbar.Brand>
+            {/* Hamburger Menu Toggle */}
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            {/* Collapsible Navbar Menu */}
+            <Navbar.Collapse
+              id="basic-navbar-nav"
+              className="justify-content-end"
+            >
+              <Nav>
+                <Nav.Link as={Link} to="/" className="text-dark">
+                  Home
+                </Nav.Link>
+                <Nav.Link as={Link} to="/about" className="text-dark">
+                  About Us
+                </Nav.Link>
+                <Nav.Link as={Link} to="/services" className="text-dark">
+                  Services
+                </Nav.Link>
+                <Nav.Link as={Link} to="/contact" className="text-dark">
+                  Contact
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
         </Row>
-
         {/* Content */}
         <Row>
           <Outlet />
         </Row>
+        {/* Footer */}
+        {/* <Row>
+          <div className="custom-margin">
+            <footer className="text-center bg-light text-dark py-3 fixed-bottom">
+              <p>
+                &copy; {new Date().getFullYear()} Druid. All rights reserved.
+              </p>
+            </footer>
+          </div>
+        </Row> */}
       </Container>
     </div>
   );
