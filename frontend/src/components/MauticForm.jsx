233d353cd4e7e9dea89dc297ebec61a1c86563f8
Suyel Bishnu <suyel2007@gmail.com>
2024-11-20 10:40:27 +0200

    Fix issue <h2> cannot appear as a descendant of <p>

--- frontend/src/components/MauticForm.jsx
+++ frontend/src/components/MauticForm.jsx
@@ -3,13 +3,13 @@ import React from "react";
 const MauticForm = () => {
   return (
     <div>
-      <h2>Contact Us</h2>
+      <h2 style={{ textAlign: "center" }}>Contact Us</h2>
+      <h6 style={{ color: "#E11000" }}>Fill out the form</h6>
       <iframe
         src="//mautic-lando.lndo.site/form/4"
         width="300"
         height="500"
         frameBorder="0"
-        allowTransparency="true"
         title="Mautic Form"
       >
         <p>Your browser does not support iframes.</p>
