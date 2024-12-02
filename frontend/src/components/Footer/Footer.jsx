fb20b3bdb38601dd13f3889d932f40813a91f40c
Your Name <85418443+alrammahi-mariia@users.noreply.github.com>
2024-12-02 18:12:24 +0200

    update footer styling

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -14,87 +14,64 @@ import styles from "./Footer.module.css";
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <Container fluid>
-        <Container className="px-3 px-md-5">
+      <Container>
+        <Row className="justify-content-between">
+          <Col md={3} sm={12} className={styles.column}>
+            <h4 className={styles.title}>Contact Us</h4>
+            <div className={styles.contactInfo}>
+              <p>
+                <FaMapMarkerAlt />
+                <span>
+                  Pasilankatu 2,
+                  <br />
+                  00240 Helsinki,
+                  <br />
+                  Finland
+                </span>
+              </p>
+              <p>
+                <FaEnvelope />
+                <span>info@druid.fi</span>
+              </p>
+              <p>
+                <FaPhone />
+                <span>+358 20 187 6602</span>
+              </p>
+            </div>
+          </Col>
+
           <div className={`${styles.verticalLine} ${styles.leftLine}`}></div>
+
+          <Col md={3} sm={12} className={styles.column}>
+            <h4 className={styles.title}>Follow Us</h4>
+            <div className={styles.socialLinks}>
+              <a href="#" target="_blank" rel="noopener noreferrer">
+                <FaFacebookSquare /> Facebook
+              </a>
+              <a href="#" target="_blank" rel="noopener noreferrer">
+                <FaLinkedin /> LinkedIn
+              </a>
+              <a href="#" target="_blank" rel="noopener noreferrer">
+                <FaInstagram /> Instagram
+              </a>
+              <a href="#" target="_blank" rel="noopener noreferrer">
+                <FaWikipediaW /> IteWiki
+              </a>
+            </div>
+          </Col>
+
           <div className={`${styles.verticalLine} ${styles.rightLine}`}></div>
 
-          <Row className="d-flex justify-content-between align-items-start">
-            <Col md={3} sm={12} className={styles.column}>
-              <h4 className={styles.title}>Contact Us</h4>
-              <div className={styles.contactInfo}>
-                <p>
-                  <FaMapMarkerAlt /> Pasilankatu 2, 00240 Helsinki, Finland
-                </p>
-                <p>
-                  <FaEnvelope /> info@druid.fi
-                </p>
-                <p>
-                  <FaPhone /> +358 20 187 6602
-                </p>
-              </div>
-            </Col>
-
-            <Col md={3} sm={12} className={styles.column}>
-              <h4 className={styles.title}>Follow Us</h4>
-              <div className={styles.socialLinks}>
-                <a
-                  href="https://www.itewiki.fi/druid"
-                  className="text-white text-decoration-none"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <FaFacebookSquare size={24} /> Facebook
-                </a>
-                <a
-                  href="https://www.itewiki.fi/druid"
-                  className="text-white text-decoration-none"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <FaLinkedin size={24} /> LinkedIn
-                </a>
-                <a
-                  href="https://www.itewiki.fi/druid"
-                  className="text-white text-decoration-none"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <FaInstagram size={24} /> Instagram
-                </a>
-                <a
-                  href="https://www.itewiki.fi/druid"
-                  className="text-white text-decoration-none"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <FaWikipediaW size={24} /> IteWiki
-                </a>
-              </div>
-            </Col>
-
-            <Col md={2} sm={12} className={styles.column}>
-              <h4 className={styles.title}>Quick Links</h4>
-              <div className={styles.quickLinks}>
-                <Link to="/about" className="text-white text-decoration-none">
-                  About Us
-                </Link>
-                <Link
-                  to="/services"
-                  className="text-white text-decoration-none"
-                >
-                  Our Services
-                </Link>
-                <Link to="/career" className="text-white text-decoration-none">
-                  Careers
-                </Link>
-                <Link to="/blog" className="text-white text-decoration-none">
-                  Blog
-                </Link>
-              </div>
-            </Col>
-          </Row>
-        </Container>
+          <Col md={3} sm={12} className={styles.column}>
+            <h4 className={styles.title}>Quick Links</h4>
+            <div className={styles.quickLinks}>
+              <Link to="/about">About us</Link>
+              <Link to="/services">Services</Link>
+              <Link to="/career">Careers</Link>
+              <Link to="/blog">Blog</Link>
+            </div>
+          </Col>
+        </Row>
       </Container>
     </footer>
   );
